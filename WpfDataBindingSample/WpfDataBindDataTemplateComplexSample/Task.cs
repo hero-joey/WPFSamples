e0dc45c3e185286bad58495d3b444f9cd7950a18
hero-joey <hero.ctu@gmail.com>
2024-03-11 22:27:58 +0800

    提交数据绑定源码

---
+++ WpfDataBindingSample/WpfDataBindDataTemplateComplexSample/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataBindDataTemplateComplexSample
+{
+    internal class Task
+    {
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public int TaskPriority { get; set; }
+
+        public TaskType TaskType { get; set; }
+    }
+
+    public enum TaskType
+    {
+        Work,
+        Home
+    }
+}
---
+++ WpfDataBindingSample/WpfDataBindDataTemplateComplexSample/TaskViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDataBindDataTemplateComplexSample
+{
+    internal class TaskViewModel : ObservableCollection<Task>
+    {
+        public TaskViewModel()
+        {
+            Add(new Task { TaskName = "Task 1", TaskDescription = "Task 1 Description", TaskPriority = 1, TaskType = TaskType.Work });
+            Add(new Task { TaskName = "Task 2", TaskDescription = "Task 2 Description", TaskPriority = 2, TaskType = TaskType.Home });
+            Add(new Task { TaskName = "Task 3", TaskDescription = "Task 3 Description", TaskPriority = 3, TaskType = TaskType.Work });
+            Add(new Task { TaskName = "Task 4", TaskDescription = "Task 4 Description", TaskPriority = 4, TaskType = TaskType.Home });
+            Add(new Task { TaskName = "Task 5", TaskDescription = "Task 5 Description", TaskPriority = 5, TaskType = TaskType.Work });
+        }
+    }
+}
